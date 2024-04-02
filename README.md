7bc4d708bf80a3cbbca0b1cbcd7dad7486f4b529
ragil.gilang <ragil.gilang@kredito.id>
2024-04-02 09:28:03 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+step step how to run this project 
+
+
+1. run backend 
+    - `./sample-backend` and follow step by  step on `./sample-backend/README.md`
+    the server will run on port : `10011`
+
+2. run frontend
+    - run command `npm install` for installing package
+    - run command `ng serve` for running web application
+    the server will run on port : `4200`
--- sampe-backend/server.js
+++ sampe-backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // Middleware untuk mengizinkan CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4300'); 
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
